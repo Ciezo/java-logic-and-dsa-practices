062e706380618c84bb026951f7387016c5adde36
Cloyd Van <abcd123.letters@gmail.com>
2024-01-08 16:17:29 +0800

    Imma get back to this tomorrow

--- src/main/java/dsa/Arrays.java
+++ src/main/java/dsa/Arrays.java
@@ -1,5 +1,31 @@
 package main.java.dsa; 
 
+import java.util.Scanner;
+
+/**
+ * This is to review and explore the primitive data types, Arrays 
+ * using the Java programming language
+ */
+
 public class Arrays {
+    private static Scanner sc = new Scanner(System.in); 
+
+    private static String[] myArrStr; 
+    private static String[] myInitArrStr2 = {"Hello, World!", 
+                                "Sugondese", 
+                                "Cheese nuts", 
+                                "Guacamole",
+                                "Watermelon"};
     
+    private static int[] myArrInt; 
+    private static int[] myArrInitInt2 = {4, 5, 6, 7, 8}; 
+
+    private static char[] myCharArr;
+    private static char[] myCharArrInit2 = {'a', 'b', 'c'};
+
+    public static void main(String[] args) {
+        System.out.println("Enter sequences");
+        System.out.print(">> ");
+
+    }
 }
