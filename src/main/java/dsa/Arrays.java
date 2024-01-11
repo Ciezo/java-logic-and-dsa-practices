fbaec092a137904cba8ff8e296ec6e6750f18944
Cloyd Van <abcd123.letters@gmail.com>
2024-01-11 14:44:22 +0800

    Just rearranged stuff...

--- src/main/java/dsa/Arrays.java
+++ src/main/java/dsa/Arrays.java
@@ -4,28 +4,33 @@ import java.util.Scanner;
 
 /**
  * This is to review and explore the primitive data types, Arrays 
- * using the Java programming language
+ * using the Java programming language.
+ * 
+ * The point of this Class file is to explore different ways to 
+ * initialize, assign, and declare arrays in Java with their 
+ * respective elements.
  */
 
 public class Arrays {
     private static Scanner sc = new Scanner(System.in); 
+    private static String tempInputHolder = ""; 
 
-    private static String[] myArrStr; 
+    private final static int SIZE = 5;
+
+    private static String[] myArrStr = new String[SIZE]; 
+    private static int[] myArrInt = new int[SIZE]; 
+    private static char[] myCharArr = new char[SIZE];
+
     private static String[] myInitArrStr2 = {"Hello, World!", 
                                 "Sugondese", 
                                 "Cheese nuts", 
                                 "Guacamole",
                                 "Watermelon"};
     
-    private static int[] myArrInt; 
     private static int[] myArrInitInt2 = {4, 5, 6, 7, 8}; 
-
-    private static char[] myCharArr;
     private static char[] myCharArrInit2 = {'a', 'b', 'c'};
 
     public static void main(String[] args) {
-        System.out.println("Enter sequences");
-        System.out.print(">> ");
-
+        
     }
 }
