cc617343ab7080f7623f89ae7335b9257c438223
Cloyd Van <abcd123.letters@gmail.com>
2024-02-01 20:54:37 +0800

    Documentation updates
    I am beginning to love working hard and coding again....thank you God!
    
    This time I must always remind myself to not burn out in coding, but rather
    I must remember that I have always loved creating software solutions
    
    __________
    
    Anyways, I have added some documentation for this exercise:

--- src/main/java/logic/ReverseLinkedList.java
+++ src/main/java/logic/ReverseLinkedList.java
@@ -1,3 +1,53 @@
+/**
+ * @author Cloyd Van Secuya
+ * 
+ * <p>
+ * What is a Linked List? 
+ * 
+ * A <b>Linked List</b> is a data structure which is like an array
+ * but without a definite size. 
+ * </p>
+ * 
+ * <p>
+ * With Linked List, we can <i>dynamically</i> add elements  
+ * which helps us understand that this data structure can grow 
+ * with no definite size  
+ * </p>
+ * 
+ * > A linked list is a <b>linear data structure</b>, in which the elements 
+ * > are not stored at contiguous memory locations.
+ * > <i>Reference: https://www.geeksforgeeks.org/data-structures/linked-list/ </i>
+ * 
+ * <p>
+ * Now, we can understand that a linked list can be used
+ * to assign different values in memory locations. 
+ * </p>
+ *
+ * <p>
+ * What are the different types of Linked List?
+ * 
+ * <ol>
+ *  <li>Singly Linked List</li>
+ *  <li>Doubly Linked List</li>
+ *  <li>Circular Linked List</li>
+ *  <li>Circular Doubly Linked List</li>
+ *  <li>Header Linked List</li>
+ * </ol>
+ * </p>
+ * 
+ * <p>
+ * How can we create Linked List?
+ * 
+ * <code>LinkedList<Type> linkedList = new LinkedList<>();</code>
+ * </p>
+ * 
+ * 
+ * <p>
+ * For this program, I will be using Integer Linked List so, I can
+ * create follow the requirement.
+ * </p> 
+ * 
+ */
 package main.java.logic; 
 
 /**
