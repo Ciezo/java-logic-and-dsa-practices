2a2325f5da13d0c342c58b3e19899ab3886b3c83
Cloyd Van <abcd123.letters@gmail.com>
2024-02-05 07:43:04 +0800

    For the mean time here is my ideas and implementation
    However, the findLargest(int[] array) is unoptimized

--- src/main/java/logic/FindLargestElementInArr.java
+++ src/main/java/logic/FindLargestElementInArr.java
@@ -1,3 +1,11 @@
+/**
+ * @author Cloyd Van Secuya
+ * 
+ * <p>
+ * In this program, I decided to use a 2 basic for-loops to 
+ * scan the given array to the function findLargest(int[] array). 
+ * </p>
+ */
 package main.java.logic; 
 
 /**
@@ -12,7 +20,25 @@ package main.java.logic;
  */
 
 public class FindLargestElementInArr {
+    
+    private static int[] array = {9, 7, 1, 3, 4, 6};
+
+    private static int findLargest(int[] array) {
+        /* Assume here max is 0 as we did not find it yet */
+        int max = 0;
+        int length = array.length;
+        for(int i = 0; i < length; i++) {
+            for(int j = i + 1; j < length - 1; j++) {
+                // Compare elements
+                if(array[i] > array[j]) max = array[i];
+                if(array[j] > array[i]) max = array[j];
+            }
+        }
+        return max;
+    }
+
     public static void main(String[] args) {
-         
+         int result = findLargest(array);
+         System.out.println(result);
     }
 }
