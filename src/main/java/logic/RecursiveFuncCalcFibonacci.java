f2ac89b37c11f5ff21b74e4d4fe8d2952f1967ac
Cloyd Van <abcd123.letters@gmail.com>
2024-02-10 03:33:58 +0800

    Updates and major changes in implementation

--- src/main/java/logic/RecursiveFuncCalcFibonacci.java
+++ src/main/java/logic/RecursiveFuncCalcFibonacci.java
@@ -24,27 +24,48 @@ package main.java.logic;
 
 public class RecursiveFuncCalcFibonacci {
 
-    private static final int SIZE = 100;
-    private static int[] fibonacciSeries = new int[SIZE];
+    private static final int SIZE = 30;
+    /** Assign here the final computed Fibonacci Series using the method, {@link #fibonacci(int startElement)}  
+     * @note Use it in the main()
+    */ private static int[] fibonacciSeries = new int[SIZE];
 
 
 
     /**
-     * 
+     * @note We cannot use a starting element beyond 2
      * @param startElement this acts as a "starting point" for the fibonacci series to generate from 
      * @return int[] fibonacciSeries
      * @example fibonacci(2)
      *          Output: 2, 3, 5, 8, 13....
      */
     private static int[] fibonacci(int startElement) {
-        fibonacciSeries = fibonacci(startElement);
-        return fibonacciSeries;
+        // Input validation 
+        /* Constraint: n >= 0 <= 2 */
+        if(startElement < 0 || startElement > 2) {
+            System.out.println("Invalid starting element!");
+            System.out.println("Please, enter 0 to 2!");
+            return new int[0];
+        }
+
+        int[] tempFib = new int[SIZE];
+
+        tempFib[0] = startElement;  
+        tempFib[1] = startElement + 1;  
+        for(int i = 2; i < SIZE-1; i++) {
+            tempFib[i] = tempFib[i-1] + tempFib[i+1];
+        }
+            
+        return tempFib;
     }
 
 
 
 
     public static void main(String[] args) {
-         
+        System.out.println("Testing...");
+        fibonacciSeries = fibonacci(2);
+        for(int i = 0; i < fibonacciSeries.length; i++) {
+            System.out.println(fibonacciSeries[i]);
+        }
     }
 }
