a7eb2e1e426eae2d048dad063cf28d029766d4ab
Cloyd Van <abcd123.letters@gmail.com>
2024-02-10 02:26:15 +0800

    Update documentation about Fibonacci
    A little introduction about Fibonacci

--- src/main/java/logic/RecursiveFuncCalcFibonacci.java
+++ src/main/java/logic/RecursiveFuncCalcFibonacci.java
@@ -1,3 +1,17 @@
+/**
+ * @author Cloyd Van Secuya
+ * 
+ * <p>
+ * What is a fibonacci sequence?
+ * It is a series of numbers denoted by, n and j, where n and j are both numbers
+ * such that n + j = x...where x is the resulting sum by n and j.  
+ * </p>
+ * 
+ * <p>
+ * Example: 
+ * 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89... 
+ * </p> 
+ */
 package main.java.logic; 
 
 /**
