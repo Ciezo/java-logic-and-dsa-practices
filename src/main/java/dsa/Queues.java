d43b3951a01bb375b976bd9d5980ef4bbbb50494
Cloyd Van <abcd123.letters@gmail.com>
2024-02-18 01:58:40 +0800

    Documentation and review about Queue data structure

--- src/main/java/dsa/Queues.java
+++ src/main/java/dsa/Queues.java
@@ -1,3 +1,27 @@
+/**
+ * @author Cloyd Van Secuya
+ * 
+ * <p>
+ * <b>Queues</b> follow the "First-In First-Out" (FIFO) data structure.
+ * </p>
+ * 
+ * <p>
+ * It is a linear data structure that is open on both ends (head and tail)
+ * where a user can perform operations accordingly such as inserting an element. 
+ * </p>
+ * 
+ * <p> 
+ * A Queue is defined as a linear data structure that is open at both ends 
+ * and the operations are <b>performed in First In First Out (FIFO) order</b>.
+ * </p> 
+ * 
+ * 
+ * <u>Common operations in a queue</u>
+ * <ol>
+ *  <li><Enqueue - it happens at the "tail" or end of the queue where an element is inserted./li>
+ *  <li>Dequeue - it happens at the "head" of the queue where an element is removed.</li>
+ * </ol>
+ */
 package main.java.dsa;
 
 public class Queues {
