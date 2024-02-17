f6b4bd42cd8e029e97723e3d0fb95ebc741005a9
Cloyd Van <abcd123.letters@gmail.com>
2024-02-18 03:27:34 +0800

    Q12 Solution and Ideas

--- src/main/java/logic/QueueUsingTwoStacks.java
+++ src/main/java/logic/QueueUsingTwoStacks.java
@@ -1,3 +1,44 @@
+/**
+ * @author Cloyd Van Secuya
+ * 
+ * <p>
+ * This exercise will help me to use two stacks and create a queue from that.
+ * Given two stacks, <code>StackA</code> and <code>StackB</code>, use
+ * their given indices, 0 to 10, to access the respective elements, 
+ * and put them into a queue.  
+ * </p>
+ * 
+ * <p>
+ * Constraint: 
+ * - StackA has size of 5, type int[]  
+ * - StackB has size of 5, type int[]
+ * - MyQueue has size of 10, type int[]
+ * </p>
+ * 
+ * <p>
+ * Example inputs:
+ * push(1, StackA)
+ * push(2, StackA)
+ * push(3, StackA)
+ * ... 
+ * push(4, StackB)
+ * push(5, StackB)
+ * push(6, StackB)
+ * 
+ * ... 
+ * 
+ * enqueue(1, StackA)
+ * enqueue(2, StackA)
+ * enqueue(3, StackA)
+ * ...
+ * </p>
+ * 
+ * <p>
+ * Explanation: 
+ * From the example input, we can see StackA and StackB are both used to populate (enqueue) the MyQueue. 
+ * Implement this concept or idea as my practice.
+ * </p>
+ */
 package main.java.logic; 
 
 /**
