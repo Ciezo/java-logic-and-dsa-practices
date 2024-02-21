1e7d3569a81272d84b3669c844900e05f7480c0a
Cloyd Van <abcd123.letters@gmail.com>
2024-02-21 11:44:25 +0800

    Refactoring and remaking proper implementations
    - Review and practice what I don't know.
    - I need to remind myself that I can suck at this on first try

--- src/main/java/dsa/Queues.java
+++ src/main/java/dsa/Queues.java
@@ -20,6 +20,8 @@
  * <ol>
  *  <li><Enqueue - it happens at the "tail" or end of the queue where an element is inserted./li>
  *  <li>Dequeue - it happens at the "head" of the queue where an element is removed.</li>
+ *  <li>isEmpty - checks if the queue is empty</li>
+ *  <li>isFull - checks if the queue is full</li>
  * </ol>
  * 
  * <p>
@@ -42,95 +44,36 @@ package main.java.dsa;
 
 public class Queues {
 
-    private static int length = 10;
-    private static int head = 0;
-    private static int tail = -1;
-    private static int[] myQueue = new int[length]; 
+    private static int SIZE = 10;
+    private static int front = 0;
+    private static int rear = -1;
+    private static int[] myQueue = new int[SIZE]; 
 
 
-
-    /**
-     * Checks if the user queue is empty. 
-     * And, the tail pointer is -1. 
-     * @return true if empty, otherwise false;
-     * @example boolean check = isEmpty(myQueue); 
-     */
     private static boolean isEmpty() {
-        if(tail == -1) 
-            return true;
+        return false;
+    }
+
+    private static boolean isFull() {   
         return false;
     }
 
 
 
-    /**
-     * Check if the queue is full, by that it means 
-     * the elements inside the queue has exceeded 
-     * the defined user length.
-     * @param queue the user's queue
-     * @return true if user's queue is full, otherwise false
-     */
-    private static boolean isFull(int[] queue) {   
-        if (queue.length > length) {
-            /** Change value of tail to 1 since there is no space left */
-            tail = 1;
-            return true;
-        }
-        return false;
+    
+    private static void enqueue() {
+        
     }
 
 
-
-    /**
-     * Inserting elements at the rear of queue.
-     * @param element to insert of type int
-     * @param queue the declared user's queue to use from
-     * @return user's queue with the most recently inserted element
-     */
-    private static int[] enqueue(int element, int[] queue) {
-        if((isFull(queue)) && isEmpty()) {
-            queue[tail+length] = element;
-            head = queue[tail];
-            tail = -1;
-        } else {
-            System.out.println("Queue is full or empty ! Cannot insert elements.");
-            return new int[0];
-        }
-        return queue;
-    }
-
-
-    /**
-     * Removing elements at the head or the front. 
-     * In removing the elements from the queue, 
-     * a value of 0 must be substituted.
-     * @param queue the user's queue.
-     * @return int[] queue with the remaining elements. 
-     */
-    private static int[] dequeue(int[] queue){
-        if(isEmpty()) {
-            head = 0;
-            queue[head] = 0;
-        } else {
-            System.out.println("Queue is empty! Cannot remove elements.");
-            return new int[0];
-        }
-        return queue;
+    private static void dequeue(){
+        
     }
 
 
 
     public static void main(String[] args) { 
-        enqueue(3, myQueue);
-        enqueue(6, myQueue);
-        enqueue(9, myQueue);
-
-        /**
-         * Check the user queue here
-         */
-        System.out.println("Maximum length of the queue: " + length);
-        System.out.println(myQueue[head]);
-        System.out.println(myQueue[tail+length]);
+        
     }
     
 }
