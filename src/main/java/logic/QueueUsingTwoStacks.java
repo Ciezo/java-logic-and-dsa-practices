08d4534c4d19578ddfe46d3fe81ba600e48f325a
Cloyd Van <abcd123.letters@gmail.com>
2024-03-13 09:04:13 +0800

    Finished implementing a queue from two stacks

--- src/main/java/logic/QueueUsingTwoStacks.java
+++ src/main/java/logic/QueueUsingTwoStacks.java
@@ -65,6 +65,11 @@ public class QueueUsingTwoStacks {
     private static int STACK_SIZE_A = 5;
     private static int STACK_SIZE_B = 5;
 
+    private static int QUEUE_SIZE = 10;
+    private static int front = -1;      // front of the queue (index 0, where -1 + 1 = 0) 
+    private static int back = -1;       // back of the queue (a.k.a as tail)
+    private static int[] queue = new int[QUEUE_SIZE];
+
     /**
      * @return 5 numbers of <int> type as data elements to use
      */
@@ -79,7 +84,78 @@ public class QueueUsingTwoStacks {
         return dataToReturn;
     }
 
-    public static void main(String[] args) {
+    /**
+     * Checks if the queue is empty meaning both 
+     * front and back points are at initial values of -1 
+     * @return true if empty, otherwise false.
+     */
+    private static boolean isEmpty() {
+        // The queue is empty if both front and back are at default values
+        // meaning they are -1.
+        if(front == -1 && back == -1) {
+            return true;
+        } 
+        return false;
+    }
+
+    /**
+     * Checks if the queue is full if we cannot insert elements at the back.
+     * @return true if full, otherwise false.
+     */
+    private static boolean isFull() {
+        // The queue is full if an element cannot be inserted at the back.
+        // Remember this, a queue is FIFO! And, elements are always inserted at the back
+        // while elements the front are first to be removed
+        if(back == QUEUE_SIZE - 1) {
+            return true;
+        } 
+        return false;
+    }
+
+    /**
+     * Print out the queue
+     */
+    public static void display() {
+        if(isEmpty()) {
+            System.out.println("No items to display!");
+        } else {    
+            for(int i = 0; i <= back; i++) {
+                System.out.println(queue[i]);
+            }
+        }
+        
+    }
+
+    /**
+     * Insert an element at the back (-1) of the queue.  
+     * @param queue an array to store the queue
+     * @param element of type int
+     */
+    private static void enqueue(int element) {
+        if(isEmpty()) {
+            // Set both front and back pointers to 
+            // point at the current element
+            back = 0;
+            front = 0;  
+            queue[back] = element;
+        } else if(isFull()) {
+            System.out.println("Queue is full");
+        } 
+        /** If the queue is not empty then, 
+         * simply keep inserting elements at the back */
+        else {
+            // Increase space at the back 
+            // to make room for more elements to be inserted
+            back++; 
+            queue[back] = element;
+            System.out.println("Back: " + queue[back]);
+
+            front = 0;      // keep pointing to the first index
+            System.out.println("Front: " + queue[front]);
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
         Stack s; 
         int[] elem = data();
 
@@ -110,5 +186,22 @@ public class QueueUsingTwoStacks {
          * @TODO 
          * - Create queue from the two stacks.
          */
+        System.out.println("Creating a queue from Stack A");
+        for(int i = 0; i < STACK_A.length; i++) {
+            enqueue(STACK_A[i]);
+        }
+
+        Thread.sleep(1000);
+
+        System.out.println("Checking queue made from Stack A");
+        display();
+
+
+        Thread.sleep(1000);
+
+        System.out.println("Creating a queue from Stack B");
+        for(int i = 0; i < STACK_A.length; i++) {
+            enqueue(STACK_B[i]);
+        }
     }
 }
