4d7aee48c299e3ee11e8d1ef6d9a7f8c78ac51de
Cloyd Van <abcd123.letters@gmail.com>
2024-07-09 15:43:00 +0800

    Find duplicate element in array

---
+++ simple_exercises/FindDuplicateNumberInArr.java
@@ -0,0 +1,63 @@
+package simple_exercises;
+
+/**
+ * Find duplicate number in an array by comparing left and right elements
+ */
+public class FindDuplicateNumberInArr {
+    
+    /**
+     * Returns the duplicate element from the array
+     * @param arr an integer type of array 
+     * @return the duplicate element, otherwise -1
+     */
+    public static int findDuplicate(int[] arr) {
+        int left, right, duplicate;
+
+        // Scan each element of the array
+        for (int i = 0; i < arr.length - 1; i++) {          // stops at length - 1 so it won't go overbound
+            left = arr[i];              // Store the left element at i
+            right = arr[i+1];           // Store the right element at i + 1
+
+            // Compare left and right elements 
+            if (left == right) {
+                duplicate = left;
+                return duplicate;
+            }
+        }
+
+        return -1; 
+    }
+    
+
+    public static int findDuplicateFromTwoArr(int[] arr_X, int[] arr_Y) {
+        int duplicate;
+
+        // Scan two arrays 
+        for (int i = 0; i < arr_X.length; i++) {
+            for (int j = 0; j < arr_Y.length; j++) {
+                // Compare each element from top to bottom
+                // Array_X      ....Scanning 
+                // Array_Y      ....Scanning
+                if (arr_X[i] == arr_Y[j]) {
+                    duplicate = arr_X[i];
+                    return duplicate;
+                }
+                
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 5};
+        int[] arr2 = {2, 6, 7, 8, 9, 10};
+        
+        // Get the duplicate from a single array
+        System.out.println(findDuplicate(arr));
+        
+        // Get the duplicate element between two arrays
+        System.out.println(findDuplicateFromTwoArr(arr, arr2));
+    }
+
+}
