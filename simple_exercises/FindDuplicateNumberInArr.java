f81568c50fd62618f0ba08bcbfc66182ca6b377b
Cloyd Van <abcd123.letters@gmail.com>
2024-07-10 14:52:46 +0800

    Input validation for arrays
    Check first if the array has contents

--- simple_exercises/FindDuplicateNumberInArr.java
+++ simple_exercises/FindDuplicateNumberInArr.java
@@ -5,6 +5,20 @@ package simple_exercises;
  */
 public class FindDuplicateNumberInArr {
     
+
+    /**
+     * Checks if an integer array is empty 
+     * @param arr an array of integer type
+     * @return true if the array has elements, otherwise false
+     */
+    public static boolean checkArray(int[] arr) {
+        int length = arr.length;
+        if (length < 1) 
+            return false;
+        
+        return true;
+    }
+
     /**
      * Returns the duplicate element from the array
      * @param arr an integer type of array 
@@ -13,18 +27,23 @@ public class FindDuplicateNumberInArr {
     public static int findDuplicate(int[] arr) {
         int left, right, duplicate;
 
-        // Scan each element of the array
-        for (int i = 0; i < arr.length - 1; i++) {          // stops at length - 1 so it won't go overbound
-            left = arr[i];              // Store the left element at i
-            right = arr[i+1];           // Store the right element at i + 1
-
-            // Compare left and right elements 
-            if (left == right) {
-                duplicate = left;
-                return duplicate;
+        if (checkArray(arr)) {
+            // Scan each element of the array
+            for (int i = 0; i < arr.length - 1; i++) {          // stops at length - 1 so it won't go overbound
+                left = arr[i];              // Store the left element at i
+                right = arr[i+1];           // Store the right element at i + 1
+    
+                // Compare left and right elements 
+                if (left == right) {
+                    duplicate = left;
+                    return duplicate;
+                }
             }
+        } else {
+            System.out.println("Array is empty!");
+            return -1;
         }
-
+        
         return -1; 
     }
     
@@ -32,18 +51,23 @@ public class FindDuplicateNumberInArr {
     public static int findDuplicateFromTwoArr(int[] arr_X, int[] arr_Y) {
         int duplicate;
 
-        // Scan two arrays 
-        for (int i = 0; i < arr_X.length; i++) {
-            for (int j = 0; j < arr_Y.length; j++) {
-                // Compare each element from top to bottom
-                // Array_X      ....Scanning 
-                // Array_Y      ....Scanning
-                if (arr_X[i] == arr_Y[j]) {
-                    duplicate = arr_X[i];
-                    return duplicate;
+        if (checkArray(arr_X) && checkArray(arr_Y)) {
+            // Scan two arrays 
+            for (int i = 0; i < arr_X.length; i++) {
+                for (int j = 0; j < arr_Y.length; j++) {
+                    // Compare each element from top to bottom
+                    // Array_X      ....Scanning 
+                    // Array_Y      ....Scanning
+                    if (arr_X[i] == arr_Y[j]) {
+                        duplicate = arr_X[i];
+                        return duplicate;
+                    }
+                    
                 }
-                
             }
+        } else {
+            System.out.println("Array is empty!");
+            return -1;
         }
 
         return -1;
