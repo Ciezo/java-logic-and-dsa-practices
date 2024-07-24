b43803589026925c031ff5237c2f3bbb2ed452a0
Cloyd Van <abcd123.letters@gmail.com>
2024-07-24 11:33:07 +0800

    Selection sort

---
+++ sorting/SelectionSort.java
@@ -0,0 +1,58 @@
+public class SelectionSort {
+    
+
+
+    /**
+     * Sort the elements of the array using Selection Sort.
+     * Selects the lowest value in the array, and moves it in front of the array. 
+     * @param arr 
+     * @return
+     */
+    private static int[] selectionSort(int[] arr) {
+        int n = arr.length;
+        int temp = 0;
+
+        /** Iterate through each element */
+        for (int i = 0; i < (n-1); i++) {
+            /** Finding the minimum value by i+1 */
+            for (int j = i+1; j < (n-1); j++) {
+                int min = i;
+                if (arr[j] < arr[min]) {        // Compare each element such that, arr[i+1] < arr[i]
+                    // Assign the index of the minimum value 
+                    min = j;
+                }
+
+                // Swap the minimum value index to the current index i
+                temp = arr[min];
+                arr[min] = arr[i];
+                arr[i] = temp;
+            }
+        }
+        
+        return arr;
+    }
+
+
+
+    /**
+     * Display an array in a single line
+     * @param arr of type int
+     */
+    private static void display(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+
+
+    public static void main(String[] args) {
+        Arrays arr = new Arrays(8);
+        
+        int[] unsortedArr = arr.populateArray();
+        int[] sortedArr = selectionSort(unsortedArr);
+        display(sortedArr);
+        
+    }
+}
