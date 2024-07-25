ebee8864f4dc9555590dd7159af295792e91701d
Cloyd Van <abcd123.letters@gmail.com>
2024-07-25 17:06:04 +0800

    Implementing quicksort
    1. Choose a pivot element from the array
    2. Compare the pivot element against current element, arr[i], to perform swapping
    3. The pivot element has a lower bound in its left side where all lower values must be inserted
    4. The higher bound of a pivot element is in the right side where all higher values must be inserted
    5. Choosing a pivot element may be from the first element, last element, middle element, or random element

---
+++ sorting/Quicksort.java
@@ -0,0 +1,15 @@
+public class Quicksort {
+    
+    private static int partition(int[] arr, int lowerBound, int higherBound) {
+        return 0;
+    }
+
+
+    private static int[] quickSort(int[] arr) {
+        return arr;
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+}
