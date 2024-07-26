e6042b55b819c812bf8fc6bd68daa1ac1e312f73
Cloyd Van <abcd123.letters@gmail.com>
2024-07-26 22:12:54 +0800

    About merge sort
    Merge sort is a divide-and-conquer sorting algorithm which takes unsorted array and creating two sub-arrays until each element has its own array to sort.
    
    Once both sub-arrays are sorted then, they are merged back together

---
+++ sorting/MergeSort.java
@@ -0,0 +1,15 @@
+public class MergeSort {
+ 
+    public static void merge() {
+
+    }
+
+    public static void sort() {
+
+    }
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
